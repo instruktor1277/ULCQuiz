69af8642462de358a9802b896edf5dc165641552
instruktor1277 <j.mikita@mazuryairport.pl>
2026-07-29 13:51:25 +0200

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -1,1 +1,25 @@
+function checkAnswer(question) {
+const resultDiv = document.getElementById('result');
+let correct = false;
+
+// Sprawdź odpowiedzi dla pytania
+const answers = document.getElementsByName(question);
+for (let i = 0; i < answers.length; i++) {
+    if (answers[i].checked) {
+        if (question === 'q1' && answers[i].value === 'b') {
+            correct = true;
+            break;
+        }
+        // Dodaj logikę dla innych pytań
+    }
+}
+
+// Wyświetl wynik
+if (correct) {
+    resultDiv.textContent = "Poprawna odpowiedź!";
+} else {
+    resultDiv.textContent = "Niestety, to nie jest poprawna odpowiedź.";
+}
+}
+
 
